1c725344bc22f39974a936e91acbb51595918c24
Thanh Phan <thanh.phan@niteco.se>
2017-03-10 17:39:30 +0700

    Added Material2 package

--- src/my-date-picker/my-date-picker.module.ts
+++ src/my-date-picker/my-date-picker.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
+import { MaterialModule } from "@angular/material";
 import { MyDatePicker } from "./my-date-picker.component";
 import { FocusDirective } from "./directives/my-date-picker.focus.directive";
 import { InputAutoFillDirective } from "./directives/my-date-picker.input.auto.fill.directive";
 
 @NgModule({
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, MaterialModule.forRoot()],
     declarations: [MyDatePicker, FocusDirective, InputAutoFillDirective],
     exports: [MyDatePicker, FocusDirective, InputAutoFillDirective]
 })
